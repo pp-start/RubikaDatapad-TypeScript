7f8d8aefcd9377557616bb17b6aac5b138dfaecd
pp-start <serwis@pp-start.pl>
2025-08-01 16:41:29 +0200

    code update

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,20 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration.ts'
 import './styles/style.scss'
 import "@fontsource/roboto"
 
+/*
+
+export { default as Error } from './components/Error';
+export { default as Common } from './components/Common';
+export { default as Login } from './components/Login';
+export { default as Admin } from './components/Admin';
+export { default as Main } from './components/Main';
+export { default as Offline } from './components/Offline';
+export { default as Schedules } from './components/Schedules';
+
+*/
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);serviceWorkerRegistration.register();
